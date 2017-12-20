84b3660d6c877ed4aeb63338f5b2fb3b89f75582
codeLocker <codeLockers@outlook.com>
2017-12-20 23:02:15 +0800

    添加NSString+ESPinYin

---
+++ ESCategory/NSString/NSString+ESPinYin.h
@@ -0,0 +1,13 @@
+//
+//  NSString+ESPinYin.h
+//  ESCategory
+//
+//  Created by codeLocker on 2017/12/20.
+//  Copyright © 2017年 codeLocker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (ESPinYin)
+- (NSString *)es_pinyin;
+@end
---
+++ ESCategory/NSString/NSString+ESPinYin.m
@@ -0,0 +1,24 @@
+
+//
+//  NSString+ESPinYin.m
+//  ESCategory
+//
+//  Created by codeLocker on 2017/12/20.
+//  Copyright © 2017年 codeLocker. All rights reserved.
+//
+
+#import "NSString+ESPinYin.h"
+
+@implementation NSString (ESPinYin)
+- (NSString *)es_pinyin {
+    if (self.length == 0) {
+        return @"";
+    }
+    NSMutableString *mutableString = [NSMutableString stringWithString:@"好的"];
+    //汉字转成拼音
+    CFStringTransform((CFMutableStringRef)mutableString, NULL, kCFStringTransformToLatin, false);
+    //去掉音调
+    CFStringTransform((CFMutableStringRef)mutableString, NULL, kCFStringTransformStripDiacritics, false);
+    return [mutableString copy];
+}
+@end
