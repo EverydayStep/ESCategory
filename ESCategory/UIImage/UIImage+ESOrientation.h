125692cc7a309b41df2515a9da5e1c29a09c5692
codeLocker <codeLockers@outlook.com>
2018-04-09 13:27:59 +0800

    commit

---
+++ ESCategory/UIImage/UIImage+ESOrientation.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+ESOrientation.h
+//  ESCategory
+//
+//  Created by codeLocker on 2018/4/9.
+//  Copyright © 2018年 codeLocker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ESOrientation)
++ (UIImage *)es_fixOrientation:(UIImage *)image;
+@end
